c482f3b14e1ef34fffc8e859392a5a1c60469191
Shamil <Shamilllaa@mail.ru>
2020-07-03 23:09:14 +0500

    'lessonCompl'

--- js/script.js
+++ js/script.js
@@ -27,6 +27,7 @@ class Todo {
 		li.innerHTML = `
 			<span class="text-todo">${item.value}</span>
 			<div class="todo-buttons">
+			<button class="todo-edit"></button>
 			<button class="todo-remove"></button>
 			<button class="todo-complete"></button>
 			</div>`;
@@ -59,34 +60,84 @@ class Todo {
 		return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 	}
 
-	deleteItem(target){
+	getItem(target, classItem, idBtn){
+		let count =0;
 		const todoItem = document.querySelectorAll('.todo-item');
 		todoItem.forEach((item) => {
-			const btn = item.querySelector('.todo-remove');
+			const btn = item.querySelector(classItem);
 			if(btn === target){
-				this.todoData.forEach((elem, index) => {
-					if(elem.value === item.textContent.trim()){
-						this.todoData.delete(elem.key);
-					}
-				});
+				if(idBtn === 0){
+					this.animeDel(count, item);
+				} 
+				if(idBtn === 1){
+					this.animeCompl(count, item);
+				} 
+				if(idBtn === 2){
+					const span = item.querySelector('.text-todo');
+					span.setAttribute('contenteditable','false');
+					this.editItem(item, span);
+				}
+			}
+		});
+	}
+		
+	animeDel(count, item)	{
+		let requestId = requestAnimationFrame(() =>{
+			this.animeDel(count, item);
+		});
+		count += 20;
+		item.style.position = 'relative';
+		item.style.left = count + 'px';
+		if (count > 700) {
+			count = 0;
+			cancelAnimationFrame(requestId);
+			this.deleteItem(item);
+		}
+	}
+
+	animeCompl(count, item){
+		let requestId = requestAnimationFrame(() =>{
+			this.animeCompl(count, item);
+		});
+		count += 20;
+		item.style.position = 'relative';
+		item.style.opacity = 1 - 0.001*(count);
+		if(item.style.opacity < 0){
+			count = 0;
+			item.style.opacity = 0 + 0.001*(count);
+			this.completedItem(item);
+			cancelAnimationFrame(requestId);
+		}
+	}
+
+	deleteItem(item){
+		this.todoData.forEach((elem, index) => {
+			if(elem.value === item.textContent.trim()){
+				this.todoData.delete(elem.key);
 			}
 		});
 		this.render();
 	}
 
-	completedItem(target){
-		const todoItem = document.querySelectorAll('.todo-item');
-		todoItem.forEach((item) => {
-			const btn = item.querySelector('.todo-complete');
-			if(btn === target){
-				this.todoData.forEach((elem) => {
-					if(elem.value === item.textContent.trim()){
-						elem.completed = !elem.completed;
-					}
-				});
+	completedItem(item){
+		this.todoData.forEach((elem) => {
+			if(elem.value === item.textContent.trim()){
+				elem.completed = !elem.completed;
 			}
 		});
 		this.render();
+	}
+
+	editItem(item, span){
+		this.todoData.forEach((elem) => {
+			if(elem.value === item.textContent.trim()){
+				span.setAttribute('contenteditable','true');
+				span.addEventListener('input', () => {
+					elem.value = span.textContent;
+					this.addToStorage();
+				});
+			}
+		});
 	}
 
 	handler(){
@@ -94,10 +145,13 @@ class Todo {
 		todoContainer.addEventListener('click', (event) => {
 			let target = event.target;
 			if(target.matches('.todo-remove')){
-				this.deleteItem(target);
+				this.getItem(target, '.todo-remove', 0);
 			}
 			if(target.matches('.todo-complete')){
-				this.completedItem(target);
+				this.getItem(target, '.todo-complete', 1);
+			}
+			if(target.matches('.todo-edit')){
+				this.getItem(target, '.todo-edit', 2);
 			}
 		});
 	}
